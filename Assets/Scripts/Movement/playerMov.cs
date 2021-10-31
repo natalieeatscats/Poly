bf19c5520c6e80e0f3c3a6bbdb1a92794d1cc06b
Natalie Hoffman <marxwasrightallalong@gmail.com>
2021-10-31 18:59:04 +0300

    Fixes and optimizations
    Fixes:
    - Player's Z value no longer changes unpredictably when going down, which resulted in the player sprite sinking into the ground more each time the player moved down a tile
    - Sorting axis has been updated according to Unity recommendations
    
    Optimizations:
    - Code has been edited for readability
    - Tile checking has been moved from FixedUpdate method to Update and now only commences when the player is trying to move

--- Assets/Scripts/Movement/playerMov.cs
+++ Assets/Scripts/Movement/playerMov.cs
@@ -12,11 +12,10 @@ public class playerMov : MonoBehaviour
     private BoundsInt area; // area of sampled tiles
     public Tilemap tilemap;
     public GridLayout grid;
-    private Vector3 position; // the player's position in the world
     private int targetTile; // the tile that the player is moving to; defined by its height relative to the player
-    private Boolean isInReach; // whether there is a occupied tile spot within sampled area
     public int acceptableHeightDiff = 1; // the traversable difference between player position and target tile
     private float worldHeightChange; // how much we need to change the player's Y coordinate
+    private int heightDiff = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -43,44 +42,55 @@ public class playerMov : MonoBehaviour
            moveDir.Normalize();  
         }
 
+                // Define sampling area
+        if (Math.Abs(moveDir.x) > 0 || Math.Abs(moveDir.y) > 0) {
+            Vector3 position = this.transform.position; // the player's position in the world
+            Vector2 samplingOffset; // how far i the direction of movement should sampling take place
+            int playerZOffset = 1; // how much the player's Z position differs from the Z position of the floor
+            Boolean isInReach = false; // whether there is a occupied tile spot within sampled area
+            samplingOffset.x = 0.4f;
+            samplingOffset.y = 0.2f;
+
+            area.position = grid.WorldToCell(new Vector3(position.x + moveDir.x * samplingOffset.x, position.y + moveDir.y * samplingOffset.y, position.z - playerZOffset));
+            area.min = new Vector3Int(area.position.x, area.position.y, area.position.z - 2);
+            area.max = new Vector3Int(area.position.x + 1, area.position.y + 1, area.position.z + 5);
+
+            // Cycle through sampled tiles to find the heighest tile, starting with the last tile space stored in the array (which is also the heighest)
+            TileBase[] tileArray = tilemap.GetTilesBlock(area);
+            for (int i = tileArray.Length - 1; i >= 0; i--)
+            {
+                if (tileArray[i] != null) { // As soon as we reach an occupied tile spot, make it the target tile and break the loop;
+                    heightDiff = i - 2; // actual difference between player position and target tile
+                    isInReach = true;
+                    break;
+                }
+                
+                // Debug.Log($"Tile {i}: {tileArray[i]}");
+                Debug.Log($"Array Length = {tileArray.Length}");
+            }
+
+            Debug.Log(isInReach);
+            // int heightDiff = i - 1; 
+
+            if(isInReach == true && Math.Abs(heightDiff) <= acceptableHeightDiff) {
+                worldHeightChange = heightDiff * 0.32f;
+                // transform.position = new Vector3(transform.position.x, transform.position.y + worldHeightChange, transform.position.z + heightDiff);
+                Debug.Log(heightDiff);
+                transform.position = new Vector3(position.x, position.y, position.z + heightDiff);
+                Move(); // If reachable and inside acceptable difference range, allow player to move;
+
+            }
+            else {
+                // rb.velocity = Vector2.zero; // otherwise set player velocity to zero, imitating collision
+            }
+        }
+        
+
     }
 
     private void FixedUpdate() 
     {
-        // Define sampling area
-        position = this.transform.position;
 
-        area.position = grid.WorldToCell(new Vector3(position.x + moveDir.x * 0.2f, position.y + moveDir.y * 0.2f, position.z));
-        area.min = new Vector3Int(area.position.x, area.position.y, area.position.z - 2);
-        area.max = new Vector3Int(area.position.x + 1, area.position.y + 1, area.position.z + 4);
-
-        // Cycle through sampled tiles to find the heighest tile, starting with the last tile space stored in the array (which is also the heighest)
-        TileBase[] tileArray = tilemap.GetTilesBlock(area);
-        for (int i = tileArray.Length - 1; i >= 0; i--)
-        {
-            if (tileArray[i] != null) { // As soon as we reach an occupied tile spot, make it the target tile and break the loop;
-                targetTile = i;
-                isInReach = true;
-                break;
-            }
-            isInReach = false; // if no such spot is found, assume there are none for the time being and continue cycle
-            
-            // Debug.Log($"Tile {i}: {tileArray[i]}");
-        }
-
-        Debug.Log(isInReach);
-        int heightDiff = targetTile - 1; // actual difference between player position and target tile
-        Debug.Log(heightDiff);
-
-        if(isInReach == true && Math.Abs(heightDiff) <= acceptableHeightDiff) {
-            Move(); // If reachable and inside acceptable difference range, allow player to move;
-            worldHeightChange = heightDiff * 0.32f;
-            // transform.position = new Vector3(transform.position.x, transform.position.y + worldHeightChange, transform.position.z + heightDiff + 1);
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + heightDiff);
-        }
-        else {
-            // rb.velocity = Vector2.zero; // otherwise set player velocity to zero, imitating collision
-        }
         
 
     }
